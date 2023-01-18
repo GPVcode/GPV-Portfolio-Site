6a57752d761e6753a6c80e47d452549842e724ba
GPV <gvill005@gmail.com>
2023-01-18 15:31:51 -0800

    first commit

---
+++ front-end/src/components/Portfolio/Data.jsx
@@ -0,0 +1,56 @@
+import Work1 from "../../assets/Restaurant-Reservation3.png";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/WLMHome.png";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Fullstack Restaurant Reservation App",
+    category: "Fullstack",
+    link: "https://restaurant-reservation-app-navy.vercel.app/dashboard"
+  },
+  // {
+  //   id: 2,
+  //   image: Work2,
+  //   title: "App movil",
+  //   category: "Frontend",
+  // },
+  {
+    id: 3,
+    image: Work3,
+    title: "Movie Website App",
+    category: "Backend",
+    link: "https://github.com/GPVcode/We-Love-Movies-Project"
+  },
+  // {
+  //   id: 4,
+  //   image: Work4,
+  //   title: "App movil",
+  //   category: "Frontend",
+  // },
+  // {
+  //   id: 5,
+  //   image: Work5,
+  //   title: "Web design",
+  //   category: "Fullstack",
+  // },
+];
+
+// projects
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "Fullstack",
+  },
+  {
+    name: "Frontend",
+  },
+  {
+    name: "Backend",
+  },
+];
