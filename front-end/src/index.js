13001ed0cb7beb2c1b565dada5a41f0897f08f35
GPV <gvill005@gmail.com>
2023-05-19 12:38:40 -0700

    working on google tag manager hookup

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import TagManager from 'react-gtm-module';
+// import TagManager from 'react-gtm-module';
 
-const tagManagerArgs = {
-  // Google Tag Manager container ID
-  gtmId: 'GTM-W29Z22C'
-}
+// const tagManagerArgs = {
+//   // Google Tag Manager container ID
+//   gtmId: 'GTM-W29Z22C'
+// }
 
-TagManager.initialize(tagManagerArgs)
+// TagManager.initialize(tagManagerArgs)
 
 // render React app
 const root = ReactDOM.createRoot(document.getElementById("root"));
